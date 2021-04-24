e6a85fffc816e081ca0cdb6b77c9e5ac323cccf9
4ndyZ <54391621+4ndyZ@users.noreply.github.com>
2021-04-24 22:37:51 +0200

    Fixed LICENSE added UNIT-Testing
    Changes:
     * Fixed LICENSE
     * Added Unit-Testing for helper.go

---
+++ helper_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_SplitMAC(t *testing.T) {
+	// Sample data
+	sampleOUI := "FCFC48"
+	sampleMAC := "FCFC48CC3E5D"
+
+	// Expected data
+	expectedOUI := "FC-FC-48"
+	expectedMAC := "FC-FC-48-CC-3E-5D"
+
+	oui := SplitMAC(sampleOUI)
+	if (oui != expectedOUI) {
+		t.Errorf("OUI splitted incorrect. Wanted %s but got %s", expectedOUI, oui)
+	}
+
+	mac := SplitMAC(sampleMAC)
+
+	if (mac != expectedMAC) {
+		t.Errorf("MAC splitted incorrect. Wanted %s but got %s", expectedMAC, mac)
+	}
+}
